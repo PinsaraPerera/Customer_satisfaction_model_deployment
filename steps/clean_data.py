fd2fa7123c4cec85a10e804cf8619594e2c7f464
PinsaraPerera <1pawanpinsara@gmail.com>
2024-04-20 17:50:34 +0530

    create file blueprint

---
+++ steps/clean_data.py
@@ -0,0 +1,19 @@
+import logging
+import pandas as pd 
+from zenml import step 
+
+@step
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Clean the data.
+    
+    Args:
+        df: pd.DataFrame: The data to clean.
+    Returns:
+        pd.DataFrame: The cleaned data.
+    '''
+    
+    logging.info("Cleaning the data")
+    # Dropping rows with missing values
+    df = df.dropna()
+    return df
