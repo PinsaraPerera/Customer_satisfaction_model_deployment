8c4c47aeb515228b11269347cbf97ced3670ca49
PinsaraPerera <1pawanpinsara@gmail.com>
2024-04-21 13:55:52 +0530

    added deployment pipeline

---
+++ pipelines/deployment_pipeline.py
@@ -0,0 +1,72 @@
+import json
+
+# from .utils import get_data_for_test
+import os
+
+import numpy as np
+import pandas as pd
+from steps.clean_data import clean_df
+from steps.evaluation import evaluate_model
+from steps.ingest_data import ingest_df
+from steps.model_train import train_model
+from zenml import pipeline, step
+from zenml.config import DockerSettings
+from zenml.constants import DEFAULT_SERVICE_START_STOP_TIMEOUT
+from zenml.integrations.constants import MLFLOW, TENSORFLOW
+from zenml.integrations.mlflow.model_deployers.mlflow_model_deployer import (
+    MLFlowModelDeployer,
+)
+from zenml.integrations.mlflow.services import MLFlowDeploymentService
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+from zenml.steps import BaseParameters, Output
+
+docker_settings = DockerSettings(required_integrations=[MLFLOW])
+
+class DeploymentTriggerConfig(BaseParameters):
+    '''Deployed model trigger configuration'''
+    min_accuracy: float = 0.0
+
+@step
+def deployment_trigger(
+    accuracy: float,
+    config: DeploymentTriggerConfig,
+) -> bool:
+    '''Step to trigger deployment'''
+    return accuracy > config.min_accuracy
+
+class MLFlowDeploymentLoaderStepParameters(BaseParameters):
+    """MLflow deployment getter parameters
+
+    Attributes:
+        pipeline_name: name of the pipeline that deployed the MLflow prediction
+            server
+        step_name: the name of the step that deployed the MLflow prediction
+            server
+        running: when this flag is set, the step only returns a running service
+        model_name: the name of the model that is deployed
+    """
+
+    pipeline_name: str
+    step_name: str
+    running: bool = True
+
+@pipeline(enable_cache=False, settings={"docker": docker_settings})
+def continuous_deployment_pipeline(
+    data_path: str,
+    min_accuracy: float = 0.0,
+    workers: int = 1,
+    timeout: int = DEFAULT_SERVICE_START_STOP_TIMEOUT,
+):
+
+    df = ingest_df(data_path=data_path)
+    X_train, X_test, y_train, y_test = clean_df(df=df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    r2_score, rmse = evaluate_model(model=model, x_test=X_test, y_test=y_test)
+
+    deployment_decision = deployment_trigger(r2_score)
+    mlflow_model_deployer_step(
+        model=model,
+        deploy_decision=deployment_decision,
+        workers=workers,
+        timeout=timeout,
+    )
--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,7 +5,7 @@ from steps.model_train import train_model
 from steps.evaluation import evaluate_model
 
 
-@pipeline(enable_cache=True)
+@pipeline(enable_cache=False)
 def train_pipeline(data_path: str):
     df = ingest_df(data_path=data_path)
     X_train, X_test, y_train, y_test = clean_df(df=df)
